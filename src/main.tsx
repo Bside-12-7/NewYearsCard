39c51b5dade6d155b5f1241fd6c6c44ade56e17f
kkjjss <jinsukk9763@gmail.com>
2024-11-19 00:15:27 +0900

    feat: react-router-dom 세팅

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import 'galmuri/dist/galmuri.css';
+import Home from "./routes/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
