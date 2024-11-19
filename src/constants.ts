607d8452916d743fd6b1e61f310a15f361b83a32
kkjjss <jinsukk9763@gmail.com>
2024-11-20 00:01:19 +0900

    feat: 사서함 마크업

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const COLOR_SET = {
+  PRIMARY: 'var(--template-color-primary)'
+}
--- src/routes/LetterBox.tsx
+++ src/routes/LetterBox.tsx
@@ -1,6 +1,57 @@
-import { useParams, useRoutes } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { COLOR_SET } from "../constants";
+import EmptyPostBoxSvg from "../assets/empty_post_box.svg?react";
 
-export default function LetterBox(){
-  const {id} = useParams();
-  return <div>{id}</div>
+const Container = styled.div`
+  width: 536px;
+  margin-inline: auto;
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: scroll;
+`;
+
+const Title = styled.span`
+  line-height: 64px;
+  font-size: 48px;
+  font-weight: 700;
+  color: ${COLOR_SET.PRIMARY};
+`;
+
+const LetterBoxWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const EmptyPostBox = styled(EmptyPostBoxSvg)`
+  width: 268px;
+  height: 150px;
+`
+
+export default function LetterBox() {
+  const { id } = useParams();
+  return (
+    <Container>
+      <Title>홈길동의 사서함</Title>
+      <LetterBoxWrapper>
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+        <EmptyPostBox />
+      </LetterBoxWrapper>
+    </Container>
+  );
 }
